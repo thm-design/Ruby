cdd9bd0b36318fafc9c866c4b37f57de02a0d69b
Tony Myer <mediamaker@gmail.com>
2013-02-09 17:01:58 +0100

    Ruby todo app
    http://pragmaticstudio.com/blog/2010/9/23/install-rails-ruby-mac

---
+++ test.rb
@@ -0,0 +1,6 @@
+name = "Joe"  
+def name.shout  
+  "#{self.upcase}!!!!"  
+end  
+puts name.shout  
+
---
+++ todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'c4d6d690673e893b0f1f6de2a96c6370c87b943a66e257f866f6eb237bcae502967a5295f631f4bbeda3dc18f78b951d4309facce089ce388d801aec0dd6557f'
